d8438ea52d7d4d6e9c3c645720786d5f5f48d6f5
Ansh Sharma <ansh.bcm@gmail.com>
2026-09-07 23:33:01 +0530

    Time: 2 ms (44.41%), Space: 10.5 MB (9.52%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+int M= 1e9+7;
+        int dp[2001];
+
+        vector<int> prev;
+    int distinctSubseqII(string s) {
+
+        int n=s.length();
+
+        memset(dp,-1, sizeof(dp));
+
+        prev.assign(n+1,0);
+
+
+        vector <int>  lastseen(26,0);
+
+        for(int i=1 ; i <=n; i++){
+            int idx = s[i-1]-'a';
+
+            prev[i] =lastseen[idx];
+            lastseen[idx]=i;
+        }
+
+        dp[0]=1;
+
+        for(int i=1 ; i <=n; i++){
+            int total=(2*dp[i-1])%M;
+
+            if(prev[i] !=0){
+                int duplicates = dp[prev[i]-1];
+                total=(total - duplicates + M) % M;
+            
+            }
+
+            dp[i] = total;
+        }
+
+        return (dp[n] - 1 + M) % M;
+        
+
+        
+
+
+
+    }
+};
